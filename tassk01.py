e54d93d99a1f44e892c25af7a4c1aa449d7d8c37
Jasur <uktamovjasur137@gmail.com>
2026-03-13 15:13:37 +0500

    done

--- tassk01.py
+++ tassk01.py
@@ -1,5 +1,15 @@
-stop = input("What: ")
-if stop == "stop":
-    print("True")
-else:
-    print("False")
+num1 = int(input("son1: "))
+num2 = int(input("son2: "))
+amal = input("amal: ")
+
+if amal == "+":
+    print(num1 + num2)
+elif amal == "*":
+    print(num1 * num2)
+elif amal == "-":
+    print(num1 - num2)
+elif amal == "/":
+    if num2 == 0:
+        print("Nolga bolinmaydi")
+    else:
+        print(num1 / num2)
